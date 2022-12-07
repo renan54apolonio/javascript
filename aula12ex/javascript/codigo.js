23f722f1474a7b678b4b68540d9d5ae6afea9724
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2022-12-07 15:45:10 -0300

    mucha coza

---
+++ aula12ex/javascript/codigo.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    
+    
+    msg.innerHTML = `agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora < 18) { 
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = 'red'
+        
+    } else {
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = 'blue'
+    }
+}
