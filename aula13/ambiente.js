403a33a7d5cb86764048769a37133de559fd16bd
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2022-12-08 16:57:48 -0300

    i dond espeak english

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1 
+while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+var a = 1 
+do {
+    console.log(`passada ${a}`)
+    a++
+} while (a <= 10)
+
+
+ 
+for (var b = 1; b <= 10; b ++){
+    console.log(`pisão ${b}`)
+}
