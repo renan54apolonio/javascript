a5b8b6fd1d03b3c4176920109311ee1ab107e422
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2022-12-08 17:31:13 -0300

    salve saçve

---
+++ aula14ex/javascript/codigo.js
@@ -0,0 +1,29 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+        window.alert('ERRO faltam dados')
+    } else {
+        res.innerHTML = 'contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('passo invalido')
+            p = 1
+        }
+
+        if (i < f) {
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} `
+            }
+        } else{
+            for(let c = i; c <= f; c -= p){
+                res.innerHTML += `${c} `
+            }
+        }
+    }
+}
